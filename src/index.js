0fdb5e79903cef7e13a31ef51b3daf8e14fe2b51
tanghh <491331418@qq.com>
2018-05-04 17:06:59 +0800

    table组件和 下拉查询组件 1.0版本提交

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import FrameWarp from './components/FrameWarp.vue'
 import Card from './components/common/Card.vue'
 import Table from './components/common/Table.vue'
 import TableCol from './components/common/TableCol.js'
-let lgbui = { Frame, Card, FrameWarp,Table,TableCol }
+import Jgrid from './components/common/Jgrid.vue'
+import Fuzzy from './components/common/Fuzzy.vue'
+import EditableFuzzy from './components/common/EditableFuzzy.vue'
+import TableFoot from './components/common/TableFoot'
+
+let lgbui = { Frame, Card, FrameWarp,Table,TableCol,Jgrid,Fuzzy,EditableFuzzy,TableFoot }
 const install = () => {
   for (let k in lgbui) {
     window.Vue.component(lgbui[k].name, lgbui[k]);
